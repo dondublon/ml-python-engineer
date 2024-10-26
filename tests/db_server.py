ab6892b2e9a1993f0046fcec7d50b9aea1848f34
Professional <email@local>
2024-10-26 18:04:10 +0200

    Draft for everything

---
+++ tests/db_server.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+class DBServerEmulator:
+    def __init__(self):
+        self.database = pd.read_parquet('data.prq')
+        self.companies_info = pd.read_parquet('meta.prq')
+
+    def get_company_filter(self, company):
+        if company[:5] == 'joint':
+            company_filter = set(self.companies_info[self.companies_info[company] == 'true'].companyName)
+        else:
+            company_filter = {company}
+        return company_filter
+
+    def get_data(self, company):
+        company_filter = self.get_company_filter(company)
+        data = self.get_data_by(company_filter)
+        return data
+
+    def get_data_by(self, company_filter):
+        return self.database[self.database.companyName.isin(company_filter)]
