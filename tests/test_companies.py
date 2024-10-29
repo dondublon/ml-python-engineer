385253766e4d85f6ddc724f4deb107b169030395
Professional <email@local>
2024-10-29 12:58:47 +0100

    test for conmanies finished

--- tests/test_companies.py
+++ tests/test_companies.py
@@ -7,9 +7,10 @@ from companies import CompaniesManager
 class DummyServer:
     def get_companies_info(self):
         df = pd.DataFrame({
-            'jointg1': [False, True],
-            'jointg2': [False, True],
-            'companyName': ['companyA', 'companyB']
+            'jointg1': ['false', 'true', 'false'],
+            'jointg2': ['true', 'false', 'false'],
+            'jointg3': ['true', 'false', 'true'],
+            'companyName': ['companyA', 'companyB', 'companyC']
         })
         return df
 
@@ -18,9 +19,14 @@ class TestCompanies(TestCase):
     def test_get_companies_only(self):
         obj = CompaniesManager(DummyServer())
         actual = obj.get_companies(False)
-        self.assertListEqual(actual, ['companyA', 'companyB'])
+        self.assertListEqual(actual, ['companyA', 'companyB','companyC'])
 
     def test_get_companies_with_jointgs(self):
         obj = CompaniesManager(DummyServer())
         actual = obj.get_companies(True)
-        self.assertListEqual(actual, ['companyA', 'companyB', 'jointg1', 'jointg2'])
+        self.assertListEqual(actual, ['companyA', 'companyB', 'companyC', 'jointg1', 'jointg2', 'jointg3'])
+
+    def test_get_joint_companies(self):
+        obj = CompaniesManager(DummyServer())
+        actual = obj.get_jointg_companies('jointg3')
+        self.assertListEqual(actual, ['companyA', 'companyC'])
